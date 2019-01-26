ab65e4225f92dcaf2a1ec210bc232e97f8b51eb6
Akil Kumar <akil.kumar.thota@autodesk.com>
2019-01-26 14:46:55 -0500

    pods intsalled

---
+++ Services/DataService.swift
@@ -0,0 +1,42 @@
+//
+//  DataService.swift
+//  GeekChat
+//
+//  Created by Akil Kumar Thota on 1/26/19.
+//  Copyright © 2019 Akil Kumar Thota. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    
+    private init() {}
+    static let sharedInstance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
+    private var _REF_GROUPS = DB_BASE.child("groups")
+    private var _REF_FEED = DB_BASE.child("feed")
+    
+    var REF_BASE : DatabaseReference {
+        
+        return _REF_BASE
+    }
+    var REF_USERS : DatabaseReference {
+        
+        return _REF_USERS
+    }
+    var REF_GROUPS : DatabaseReference {
+        
+        return _REF_GROUPS
+    }
+    var REF_FEED : DatabaseReference {
+        
+        return _REF_FEED
+    }
+    
+    
+}
